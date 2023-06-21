d50873836929fecddc30692783e3dad798c65e3b
Andrey Sinikov <andreysinikov91@gmail.com>
2023-06-21 23:31:42 +0300

    добавляет генератор уникальных чисел

--- js/main.js
+++ js/main.js
@@ -23,8 +23,24 @@ const messages = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
+// Генератор случайных чисел
 const getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 getRandomNumber();
 
+// Генератор неповторяющихся чисел
+const getUniqueNumber = function (min, max) {
+  const usedNumbers = [];
+
+  return function () {
+    let currentNumber = getRandomNumber (min, max);
+
+    while (usedNumbers.includes(currentNumber)) {
+      currentNumber = getRandomNumber(min, max);
+    }
+    usedNumbers.push(currentNumber);
+    return currentNumber;
+  };
+};
+getUniqueNumber();
