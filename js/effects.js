da28ac75f3db9f66ab9e026368955ae1dc6fd5e3
Andrey Sinikov <andreysinikov91@gmail.com>
2023-07-21 22:39:54 +0300

    добавляет массив с параметрами эффектов

---
+++ js/effects.js
@@ -0,0 +1,54 @@
+// Массив с параметрами эффектов для изображения
+const effects = {
+  none: {
+    name: 'none',
+    style: 'none',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '',
+  },
+  chrome: {
+    name: 'chrome',
+    style: 'grayscale',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  sepia: {
+    name: 'sepia',
+    style: 'sepia',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+  marvin: {
+    name: 'marvin',
+    style: 'invert',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '%',
+  },
+  phobos: {
+    name: 'phobos',
+    style: 'blur',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    unit: 'px',
+  },
+  heat: {
+    name: 'heat',
+    style: 'brightness',
+    min: 1,
+    max: 3,
+    step: 0.1,
+    unit: '',
+  },
+};
+
+const defaultEffect = effects.none;
+let chosenEffect = defaultEffect;
