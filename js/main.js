09bde0d14fe77db98b9b5231ae810f891859b54b
Andrey Sinikov <andreysinikov91@gmail.com>
2023-07-29 23:59:20 +0300

    добавляет фильтрацию фото

--- js/main.js
+++ js/main.js
@@ -3,12 +3,15 @@ import { showAlert } from './util.js';
 import { renderPhotos } from './thumbnails.js';
 import { openModal } from './open-photo.js';
 import { hideModalForm } from './form.js';
+import { showFilters, setDebouncedFilter } from './thumbnails-filter.js';
 
 try {
   const data = await getData();
-  renderPhotos(data);
   openModal(data);
   hideModalForm();
+  showFilters();
+  renderPhotos(data);
+  setDebouncedFilter(data);
 } catch (err) {
   showAlert(err.message);
 }
